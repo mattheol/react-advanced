b2a35d28e621374d8ee4005eeaf233a388012e00
Mateusz Olsztyński <matiol1@hotmail.com>
2020-03-15 22:50:41 +0100

    UseEffect hook with axios retrieving todos from mocky.com

--- src/views/TodoWithHooks.js
+++ src/views/TodoWithHooks.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import closeIcon from "../assets/close.svg";
 import "./TodoWithHooks.css";
+import axios from "axios";
 const TodoWithHooks = () => {
   const [inputContent, setInputContent] = useState("");
-  const [taskList, setTaskList] = useState([{ id: 1, task: "Eat breakfast" }]);
+  const [taskList, setTaskList] = useState([
+    { id: 1, task: "Zjesć śniadanie" }
+  ]);
+  const [tasksMock, setTaskMock] = useState([]);
+
+  useEffect(() => {
+    const retrieveData = async () => {
+      const response = await axios.get(
+        "http://www.mocky.io/v2/5e6ea28f2f00002640a0393f"
+      );
+      setTaskMock(response.data);
+    };
+    retrieveData();
+  });
 
   const addItem = () => {
     setTaskList([...taskList, { id: taskList.length + 1, task: inputContent }]);
@@ -12,6 +26,7 @@ const TodoWithHooks = () => {
   const deleteItem = id => {
     setTaskList(taskList.filter(t => t.id !== id));
   };
+
   return (
     <div>
       <h1>Todo list</h1>
@@ -32,6 +47,15 @@ const TodoWithHooks = () => {
             alt="delete item"
             className="x-icon"
           />
+        </div>
+      ))}
+      {tasksMock.map(task => (
+        <div
+          className="todo-item"
+          style={{ backgroundColor: "green", color: "white" }}
+          key={task.id}
+        >
+          <p>{task.task}</p>
         </div>
       ))}
     </div>
